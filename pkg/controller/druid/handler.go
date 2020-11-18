9aca27a8b13c114d53d941f1713c13a708cd39f6
Himanshu <g.himanshu@gmail.com>
2020-11-18 14:39:58 -0800

    add support for ImagePullPolicy (#92)

--- pkg/controller/druid/handler.go
+++ pkg/controller/druid/handler.go
@@ -913,6 +913,7 @@ func makePodSpec(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid, nodeSpecUn
 				Image:           firstNonEmptyStr(nodeSpec.Image, m.Spec.Image),
 				Name:            fmt.Sprintf("%s", nodeSpecUniqueStr),
 				Command:         []string{firstNonEmptyStr(m.Spec.StartScript, "bin/run-druid.sh"), nodeSpec.NodeType},
+				ImagePullPolicy: v1.PullPolicy(firstNonEmptyStr(string(nodeSpec.ImagePullPolicy), string(m.Spec.ImagePullPolicy))),
 				Ports:           nodeSpec.Ports,
 				Resources:       nodeSpec.Resources,
 				Env:             getEnv(nodeSpec, m, configMapSHA),
