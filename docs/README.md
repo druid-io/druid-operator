b3601d3a057985491995ef48724c904710f0fe98
Himanshu Gupta <g.himanshu@gmail.com>
2020-11-30 16:16:49 -0800

    delete old code within operator-sdk framework

--- docs/README.md
+++
@@ -1,6 +0,0 @@
-## Druid Operator Documenation  
-
-* [Install the Operator](./getting_started.md)
-* [Druid CR Spec](./druid_cr.md)
-* [Example Specs](./examples.md)
-* [Upgrade Update and Scaling Druid Cluster](./upgrade.md)
--- pkg/apis/druid/group.go
+++
@@ -1,6 +0,0 @@
-// Package druid contains druid API versions.
-//
-// This file ensures Go source parsers acknowledge the druid package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package druid
