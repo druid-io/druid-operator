7c107325a2de121a42a3fff66f3716af844fa458
AdheipSingh <adheip.singh@rilldata.com>
2021-04-02 16:43:31 +0530

    nil for emptyslice

--- controllers/druid/handler.go
+++ controllers/druid/handler.go
@@ -938,7 +938,7 @@ func getServiceName(nameTemplate, nodeSpecUniqueStr string) string {
 }
 
 func getPersistentVolumeClaim(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid) []v1.PersistentVolumeClaim {
-	pvc := []v1.PersistentVolumeClaim{}
+	var pvc []v1.PersistentVolumeClaim
 
 	for _, val := range m.Spec.VolumeClaimTemplates {
 		pvc = append(pvc, val)
@@ -953,6 +953,7 @@ func getPersistentVolumeClaim(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Drui
 }
 
 func getVolumeMounts(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid) []v1.VolumeMount {
+
 	volumeMount := []v1.VolumeMount{
 		{
 			MountPath: firstNonEmptyStr(m.Spec.CommonConfigMountPath, defaultCommonConfigMountPath),
@@ -976,7 +977,8 @@ func getNodeConfigMountPath(nodeSpec *v1alpha1.DruidNodeSpec) string {
 }
 
 func getTolerations(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid) []v1.Toleration {
-	tolerations := []v1.Toleration{}
+
+	var tolerations []v1.Toleration
 
 	for _, val := range m.Spec.Tolerations {
 		tolerations = append(tolerations, val)
