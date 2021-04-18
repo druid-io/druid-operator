4813b57fb14acc43e154579347c15eb82c9e40a2
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2021-04-18 10:23:40 +0530

    Fix #61: Update HPA to apiVersion v2beta2 (#166)
    * updates for hpa v2beta2
    
    * update hpa examples
    
    Co-authored-by: AdheipSingh <adheip.singh@rilldata.com>

--- controllers/druid/handler.go
+++ controllers/druid/handler.go
@@ -10,7 +10,7 @@ import (
 	"regexp"
 	"sort"
 
-	autoscalev2beta1 "k8s.io/api/autoscaling/v2beta1"
+	autoscalev2beta2 "k8s.io/api/autoscaling/v2beta2"
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 
 	"github.com/druid-io/druid-operator/apis/druid/v1alpha1"
@@ -304,7 +304,7 @@ func deployDruidCluster(sdk client.Client, m *v1alpha1.Druid) error {
 	updatedStatus.HPAutoScalers = deleteUnusedResources(sdk, m, hpaNames, ls,
 		func() runtime.Object { return makeHorizontalPodAutoscalerListEmptyObj() },
 		func(listObj runtime.Object) []object {
-			items := listObj.(*autoscalev2beta1.HorizontalPodAutoscalerList).Items
+			items := listObj.(*autoscalev2beta2.HorizontalPodAutoscalerList).Items
 			result := make([]object, len(items))
 			for i := 0; i < len(items); i++ {
 				result[i] = &items[i]
@@ -1264,10 +1264,10 @@ func makePodDisruptionBudget(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid
 	return pdb, nil
 }
 
-func makeHorizontalPodAutoscaler(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid, ls map[string]string, nodeSpecUniqueStr string) (*autoscalev2beta1.HorizontalPodAutoscaler, error) {
+func makeHorizontalPodAutoscaler(nodeSpec *v1alpha1.DruidNodeSpec, m *v1alpha1.Druid, ls map[string]string, nodeSpecUniqueStr string) (*autoscalev2beta2.HorizontalPodAutoscaler, error) {
 	nodeHSpec := *nodeSpec.HPAutoScaler
 
-	hpa := &autoscalev2beta1.HorizontalPodAutoscaler{
+	hpa := &autoscalev2beta2.HorizontalPodAutoscaler{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "autoscaling/v2beta1",
 			Kind:       "HorizontalPodAutoscaler",
@@ -1406,8 +1406,8 @@ func makePodDisruptionBudgetListEmptyObj() *v1beta1.PodDisruptionBudgetList {
 	}
 }
 
-func makeHorizontalPodAutoscalerListEmptyObj() *autoscalev2beta1.HorizontalPodAutoscalerList {
-	return &autoscalev2beta1.HorizontalPodAutoscalerList{
+func makeHorizontalPodAutoscalerListEmptyObj() *autoscalev2beta2.HorizontalPodAutoscalerList {
+	return &autoscalev2beta2.HorizontalPodAutoscalerList{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "autoscaling/v2beta1",
 			Kind:       "HorizontalPodAutoscaler",
@@ -1469,8 +1469,8 @@ func makePodDisruptionBudgetEmptyObj() *v1beta1.PodDisruptionBudget {
 	}
 }
 
-func makeHorizontalPodAutoscalerEmptyObj() *autoscalev2beta1.HorizontalPodAutoscaler {
-	return &autoscalev2beta1.HorizontalPodAutoscaler{
+func makeHorizontalPodAutoscalerEmptyObj() *autoscalev2beta2.HorizontalPodAutoscaler {
+	return &autoscalev2beta2.HorizontalPodAutoscaler{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "autoscaling/v2beta1",
 			Kind:       "HorizontalPodAutoscaler",
